7d937ad51a55cabcee9c5b3fbe1b2420cdbda1b8
collectorstar <117626171+collectorstar@users.noreply.github.com>
2026-04-14 22:50:12 +0700

    first commit

---
+++ src/main/java/api/flowapprove/FlowApproveApplication.java
@@ -0,0 +1,13 @@
+package api.flowapprove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowApproveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowApproveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/flowapprove/FlowApproveApplicationTests.java
@@ -0,0 +1,13 @@
+package api.flowapprove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApproveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
